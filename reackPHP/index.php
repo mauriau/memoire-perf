113680b023fe7bc242979de0f2463570a1aee1e5
mauriau <maxime.auriau@gmail.com>
2016-02-23 21:55:30 +0100

    Add ReactPHP and NodeJS simple server

---
+++ reackPHP/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'vendor/autoload.php';
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server($loop);
+$http = new React\Http\Server($socket, $loop);
+$http->on('request', function ($request, $response) {
+    $response->writeHead(200, array('Content-Type' => 'text/plain'));
+    $response->end("Hello World aevc ReactPHP\n");
+});
+$socket->listen(1337);
+$loop->run();
